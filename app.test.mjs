2c452dd9614e55b40d0fd1f48c4853295cbdad55
Mu Vonglao <muvonglao@gmail.com>
2025-05-15 10:54:43 +0700

    fix test case message in GET /

--- app.test.mjs
+++ app.test.mjs
@@ -5,7 +5,7 @@ describe("GET /", () => {
   it("should return Hello TechUp!", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe("Hello World!");
+    expect(res.text).toBe("Hello TechUp!");
   });
 });
 
